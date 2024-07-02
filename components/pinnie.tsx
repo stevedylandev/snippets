85fa9f5657f5e432fd706aa8cf26e9e31cd87d6e
Steve <stevedsimkins@gmail.com>
2024-07-02 09:11:06 -0400

    removed image test

--- components/pinnie.tsx
+++
@@ -1,9 +0,0 @@
-export function Pinnie() {
-  return (
-    <img
-      src="https://snippets.mypinata.cloud/ipfs/QmVLwvmGehsrNEvhcCnnsw5RQNseohgEkFNN1848zNzdng"
-      alt="pinnie"
-      crossOrigin="anonymous"
-    />
-  );
-}
