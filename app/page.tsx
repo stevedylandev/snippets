219c4e5a35042328eba2e8d58c6eda3be996a97c
Steve <stevedsimkins@gmail.com>
2024-07-02 08:26:43 -0400

    image test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { CodeForm } from "@/components/code-form";
+import { Pinnie } from "@/components/pinnie";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <CodeForm readOnly={false} content="console.log('hello world!')" />
+      <Pinnie />
     </main>
   );
 }
---
+++ components/pinnie.tsx
@@ -0,0 +1,9 @@
+export function Pinnie() {
+  return (
+    <img
+      src="https://snippets.mypinata.cloud/ipfs/QmVLwvmGehsrNEvhcCnnsw5RQNseohgEkFNN1848zNzdng"
+      alt="pinnie"
+      crossOrigin="anonymous"
+    />
+  );
+}
