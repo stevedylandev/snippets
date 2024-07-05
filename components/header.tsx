c8b847217fdb346c99a4622dcdd471a574865405
Steve Simkins <73185218+stevedylandev@users.noreply.github.com>
2024-07-05 14:24:01 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="flex align-center justify-start w-[350px] sm:w-[600px] mb-4">
+    <div className="flex align-center justify-start sm:mt-0 mt-4 w-[350px] sm:w-[600px] mb-4">
       <Link className="font-commitMono text-2xl font-bold" href="/">
         Snippets.so
       </Link>
