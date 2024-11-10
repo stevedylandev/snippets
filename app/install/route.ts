381b50567e12da2189048386f8e45833ac6bc6f0
Steve <stevedsimkins@gmail.com>
2024-11-10 13:36:01 -0500

    add install route

---
+++ app/install/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+	try {
+		const data = await fetch("https://files-cli-install.vercel.app/script.sh");
+		const file = await data.blob();
+		const headers = new Headers();
+		headers.set("Content-Type", "application/octet-stream");
+		headers.set("Content-Disposition", 'attachment; filename="install.sh"');
+		return new NextResponse(file, {
+			status: 200,
+			headers: headers,
+		});
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(error);
+	}
+}
