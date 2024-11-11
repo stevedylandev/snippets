39abfb1328999d5b8eee05823e090e4a899d3e5d
Steve Simkins <73185218+stevedylandev@users.noreply.github.com>
2024-11-10 23:15:17 -0500

    Update route.ts

--- app/install/route.ts
+++ app/install/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
 	try {
 		const data = await fetch("https://www.snippets.so/script.sh");
-		const file = await data.blob();
+		const file = await data.text();
 		const headers = new Headers();
-		headers.set("Content-Type", "application/octet-stream");
+		headers.set("Content-Type", "text/plain");
 		headers.set("Content-Disposition", 'attachment; filename="install.sh"');
 		return new NextResponse(file, {
 			status: 200,
